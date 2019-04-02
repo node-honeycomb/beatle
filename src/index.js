a0e4926724e34dfdba868cd98da5c025cf379c8a
雨异 <yuyi.zsj@taobao.com>
2019-04-02 10:30:52 +0800

    react-router-dom to react-router part2

--- src/index.js
+++ src/index.js
@@ -278,7 +278,7 @@ const BeatlePro = enhancleBeatle(Beatle);
   };
 });
 
-BeatlePro.prototype.version = '2.0.0-beta.3';
+BeatlePro.prototype.version = '2.0.0-rc.2';
 module.exports = BeatlePro;
 
 export default BeatlePro;
