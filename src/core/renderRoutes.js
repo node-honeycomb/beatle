bc8b8663e8dc10093a454bef347b690262ddcb8f
雨异 <yuyi.zsj@taobao.com>
2019-04-02 14:04:09 +0800

    migrate react-router v4 getComponent

--- src/core/renderRoutes.js
+++ src/core/renderRoutes.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import {Switch, Route} from 'react-router-dom';
 
 export default function renderRoutes(routes, extraProps, switchProps) {
@@ -9,16 +9,34 @@ export default function renderRoutes(routes, extraProps, switchProps) {
     switchProps = {};
   }
   return routes ? (<Switch {...switchProps}>{routes.map(function (route, i) {
-    return (<Route
-      key={route.key || i}
-      path={route.path}
-      exact={route.exact}
-      strict={route.strict}
-      render={(props) => {
-        const newProps = Object.assign({}, props, extraProps, {route: route});
-        newProps.children = renderRoutes(route.routes, route.extraProps, route.switchProps);
-        return route.render ? route.render(newProps) : (<route.component {...newProps} />);
-      }}
-    ></Route>);
+    const routeProps = {
+      path: route.path || route.name,
+      exact: route.exact,
+      strict: route.strict
+    };
+    routeProps.render = function render(props) {
+      const newProps = Object.assign({
+        children: renderRoutes(route.routes, route.extraProps, route.switchProps)
+      }, props, extraProps, {route: route});
+      if (route.component) {
+        return (<route.component {...newProps} />);
+      } else if (route.getComponent) {
+        const Component = React.lazy(() => new Promise(resolve => {
+          route.getComponent(props, (RouteComponent, err) => {
+            if (err) {
+              resolve(err.message || 'Error by getComponent' || route.loading);
+            } else {
+              resolve(RouteComponent);
+            }
+          });
+        }));
+        return (<Suspense fallback={route.loading}>
+          (<Component {...newProps} />)
+        </Suspense>);
+      } else {
+        return route.loading;
+      }
+    };
+    return (<Route key={route.key || i} {...routeProps} />);
   })}</Switch>) : null;
 }
