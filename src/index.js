91a7c7125c72e88af8f54ad740681beeb99c36e8
雨异 <yuyi.zsj@taobao.com>
2019-04-12 19:41:18 +0800

    fix: stateByModel

--- src/index.js
+++ src/index.js
@@ -276,7 +276,7 @@ const BeatlePro = enhancleBeatle(Beatle);
   };
 });
 
-BeatlePro.prototype.version = '1.3.8';
+BeatlePro.prototype.version = '1.3.9';
 module.exports = BeatlePro;
 
 export default BeatlePro;
